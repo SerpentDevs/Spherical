1116778e4695a26dd8b3029f47bbcf6fe3eed05e
Unknown <jake@gealer.email>
2018-10-15 18:20:22 +0100

    More shit

--- docker_entrypoint_script.sh
+++ docker_entrypoint_script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Start the service
+service php7.0-fpm start
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start PHP 7.0 FPM: $status"
+  exit $status
+fi
+
 # Start the first process
 nginx
 status=$?
@@ -15,22 +23,3 @@ if [ $status -ne 0 ]; then
   echo "Failed to start my_second_process: $status"
   exit $status
 fi
-
-# Naive check runs checks once a minute to see if either of the processes exited.
-# This illustrates part of the heavy lifting you need to do if you want to run
-# more than one service in a container. The container exits with an error
-# if it detects that either of the processes has exited.
-# Otherwise it loops forever, waking up every 60 seconds
-
-while sleep 60; do
-  ps aux |grep my_first_process |grep -q -v grep
-  PROCESS_1_STATUS=$?
-  ps aux |grep my_second_process |grep -q -v grep
-  PROCESS_2_STATUS=$?
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
-    exit 1
-  fi
-done
