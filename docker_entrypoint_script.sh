69fc49f929c661805ea93414083dc80328e7ff6f
Unknown <jake@gealer.email>
2018-10-15 18:21:38 +0100

    That seemed promising

--- docker_entrypoint_script.sh
+++ docker_entrypoint_script.sh
@@ -17,7 +17,7 @@ if [ $status -ne 0 ]; then
 fi
 
 # Start the second process
-python -c "import urllib.request; urllib.request.urlopen('http://127.0.0.1:8000/gen_captcha.php').read()"
+python -c "import urllib.request; print(urllib.request.urlopen('http://127.0.0.1:8000/gen_captcha.php').read())"
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start my_second_process: $status"
